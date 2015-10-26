daac7d9396c33f00fd84c749930922d780068527
Jonghyuk Jung <heretor92@gmail.com>
2015-10-26 18:12:57 +0900

    Modified about page.

--- apps/about/views.py
+++ apps/about/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 from apps.about.models import Introduction, Pursuit, Offer
 
 def index(request):
-    intro = Introduction.objects.all().order_by('id')[0]
+    intro = Introduction.objects.all().order_by('id')
     pursuits = Pursuit.objects.all().order_by('id')
-    offer = Offer.objects.all().order_by('id')[0]
+    offer = Offer.objects.all().order_by('id')
+
+    if intro:
+        intro = intro[0]
+    else:
+        intro = ''
+
+    if offer:
+        offer = offer[0]
+    else:
+        offer = ''
 
     qs = {
         "intro": intro,
