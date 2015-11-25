0579fb50e5c86beb2b3dad8b77b46c80f4b58ff9
cixd <cixd.members@gmail.com>
2015-11-25 13:59:28 +0900

    Modified

--- apps/publication/admin.py
+++ apps/publication/admin.py
@@ -5,7 +5,7 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('order', 'name',)
 
 class PublicationAdmin(admin.ModelAdmin):
-    list_display = ('category', 'scope', 'title',)
+    list_display = ('scope', 'title',)
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Publication, PublicationAdmin)
